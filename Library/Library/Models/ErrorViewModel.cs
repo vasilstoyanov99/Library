ec47b40c884b16281b8b8fce26863f20e640c2fe
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-11-20 21:19:46 +0200

    Add empty project

---
+++ Library/Library/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace Library.Models
+{
+    using System;
+
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
