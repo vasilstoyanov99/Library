2577891d409e0f08c392ae6931d3d049bcdd21e1
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-11-21 03:23:45 +0200

    Update HomeController.cs

--- Library/Library/Controllers/HomeController.cs
+++ Library/Library/Controllers/HomeController.cs
@@ -23,11 +23,6 @@
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
