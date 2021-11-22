98c7af93fdabf9e4dd5be964aac37f81ff2a0dec
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-11-22 21:04:59 +0200

    Added Required attribute

--- Library/Library/Services/Books/Models/AddBookFormModel.cs
+++ Library/Library/Services/Books/Models/AddBookFormModel.cs
@@ -1,9 +1,11 @@
 ﻿namespace Library.Services.Books.Models
 {
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
 
     public class AddBookFormModel : EditBookFormModel
     {
+        [Required]
         public string GenreId { get; set; }
 
         public IEnumerable<GenreServiceModel> Genres { get; set; }
