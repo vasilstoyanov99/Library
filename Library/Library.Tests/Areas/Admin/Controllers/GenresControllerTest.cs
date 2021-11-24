8862609891636fb01ccdf1f7c01fb2c3afbf09ff
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-11-25 00:02:31 +0200

    Added genres controller tests with the needed data

---
+++ Library/Library.Tests/Areas/Admin/Controllers/GenresControllerTest.cs
@@ -0,0 +1,54 @@
+﻿namespace Library.Tests.Areas.Admin.Controllers
+{
+    using System.Linq;
+
+    using Xunit;
+    using Shouldly;
+    using MyTested.AspNetCore.Mvc;
+
+    using Library.Data.Models;
+    using Library.Areas.Admin.Controllers;
+
+    using static Data.DbModels.GenresControllerTestDbModels;
+    using static Library.Global.GlobalConstants.Notifications;
+    using static Library.Areas.Admin.AdminConstants;
+
+    public class GenresControllerTest
+    {
+        [Fact]
+        public void AddGenreShouldReturnView()
+        {
+            MyController<GenresController>
+                .Instance()
+                .Calling(c => c.AddGenre())
+                .ShouldReturn()
+                .View();
+        }
+
+        [Fact]
+        public void AddGenreShouldRedirectWithTempDataMessageAndSaveGenreWithValidData()
+        {
+            var addGenreFormModel = AddGenreModel;
+
+            MyController<GenresController>
+                .Instance()
+                .WithUser(u => u.InRole(AdminRoleName))
+                .Calling(c => c.AddGenre(addGenreFormModel))
+                .ShouldHave()
+                .Data(data => data
+                    .WithSet<Genre>(set =>
+                    {
+                        set.ShouldNotBeNull();
+                        set.FirstOrDefault(g => g.Name == addGenreFormModel.Name)
+                            .ShouldNotBeNull();
+                    }))
+                .AndAlso()
+                .ShouldHave()
+                .TempData(tempData => tempData
+                    .ContainingEntryWithKey(SuccessfullyAddedGenreKey))
+                .AndAlso()
+                .ShouldReturn()
+                .RedirectToAction("AddGenre");
+        }
+    }
+}
---
+++ Library/Library.Tests/Areas/Data/DbModels/GenresControllerTestDbModels.cs
@@ -0,0 +1,12 @@
+﻿namespace Library.Tests.Areas.Data.DbModels
+{
+    using Library.Areas.Admin.Services.Genres.Models;
+
+    public class GenresControllerTestDbModels
+    {
+        public static AddGenreFormModel AddGenreModel = new()
+        {
+            Name = "New Genre Test Name"
+        };
+    }
+}
