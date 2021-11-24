9423d77ca0279cd8c219770c9f7d86b6c09252a4
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-11-24 17:42:35 +0200

    Added genres controller routing tests

---
+++ Library/Library.Tests/Areas/Admin/Routing/GenresControllerTest.cs
@@ -0,0 +1,25 @@
+﻿namespace Library.Tests.Areas.Admin.Routing
+{
+    using MyTested.AspNetCore.Mvc;
+    using Xunit;
+
+    using Library.Areas.Admin.Controllers;
+    public class GenresControllerTest
+    {
+        [Fact]
+        public void GetAddGenreShouldBeMapped()
+            => MyRouting
+                .Configuration()
+                .ShouldMap("/Admin/Genres/AddGenre")
+                .To<GenresController>(g => g.AddGenre());
+
+        [Fact]
+        public void PostAddGenreShouldBeMapped()
+            => MyRouting
+                .Configuration()
+                .ShouldMap(request => request
+                    .WithPath("/Admin/Genres/AddGenre")
+                    .WithMethod(HttpMethod.Post))
+                .To<GenresController>(b => b.AddGenre(null));
+    }
+}
