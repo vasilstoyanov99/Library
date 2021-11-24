2b0e3a4f7ebaa641abf6870275d8bcab7e17c8c1
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-11-24 16:56:19 +0200

    Added home controller routing tests

---
+++ Library/Library.Tests/Routing/HomeControllerTest.cs
@@ -0,0 +1,24 @@
+﻿namespace Library.Tests.Routing
+{
+    using MyTested.AspNetCore.Mvc;
+    using Xunit;
+
+    using Library.Controllers;
+
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void IndexRouteShouldBeMapped()
+            => MyRouting
+                .Configuration()
+                .ShouldMap("/")
+                .To<HomeController>(c => c.Index());
+
+        [Fact]
+        public void ErrorRouteShouldBeMapped()
+            => MyRouting
+                .Configuration()
+                .ShouldMap("/Home/Error")
+                .To<HomeController>(c => c.Error());
+    }
+}
---
+++ Library/Library.Tests/TestStartup.cs
@@ -0,0 +1,25 @@
+﻿namespace Library.Tests
+{
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    using MyTested.AspNetCore.Mvc;
+
+    public class TestStartup : Startup
+    {
+        public TestStartup(IConfiguration configuration)
+            : base(configuration)
+        {
+        }
+
+        public void ConfigureTestServices(IServiceCollection services)
+        {
+            this.ConfigureServices(services);
+
+            // Replace only your own custom services. The ASP.NET Core ones 
+            // are already replaced by MyTested.AspNetCore.Mvc. 
+            /*services.ReplaceTransient<IHomeService, HomeService>();*/
+            services.AddMvc();
+        }
+    }
+}
