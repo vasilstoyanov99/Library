c907907842e24ee59edcc8798bc62bb557d307ed
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-11-24 22:06:56 +0200

    Added Books controller methods tests with the needed tests data

---
+++ Library/Library.Tests/Controllers/BooksControllerTest.cs
@@ -0,0 +1,228 @@
+﻿using System.Linq;
+using Library.Data.Models;
+using Shouldly;
+
+namespace Library.Tests.Controllers
+{
+    using System.Collections.Generic;
+
+    using MyTested.AspNetCore.Mvc;
+    using Xunit;
+
+    using Library.Controllers;
+    using Library.Services.Books.Models;
+
+    using static Data.DbModels.BooksControllerTestDbModels;
+    using static Library.Global.GlobalConstants.Notifications;
+    using static Library.Areas.User.UserConstants;
+    using static Library.Areas.Admin.AdminConstants;
+    public class BooksControllerTest
+    {
+        [Fact]
+        public void AllShouldReturnViewWithModelWithValidData()
+        {
+            var expectedModel = AllBooksModel;
+
+            MyController<BooksController>
+                .Instance()
+                .WithData(TestBook)
+                .Calling(c => c.All(1))
+                .ShouldReturn()
+                .View(expectedModel);
+        }
+
+        [Fact]
+        public void DetailsShouldReturnViewWithModelWithValidDataAndUserShouldBeAbleToEdit()
+        {
+            var expectedModel = BookDetailsModel;
+            expectedModel.CanUserEdit = true;
+
+            MyController<BooksController>
+                .Instance()
+                .WithUser()
+                .WithData(TestGenre, TestBook)
+                .Calling(c => c.Details(TestBook.Id))
+                .ShouldReturn()
+                .View(expectedModel);
+        }
+
+        [Fact]
+        public void DetailsShouldReturnViewWithModelWithValidDataAndUserShouldNotBeAbleToEdit()
+        {
+            var expectedModel = BookDetailsModel;
+            expectedModel.CanUserEdit = false;
+
+            MyController<BooksController>
+                .Instance()
+                .WithData(TestGenre, TestBook)
+                .Calling(c => c.Details(TestBook.Id))
+                .ShouldReturn()
+                .View(expectedModel);
+        }
+
+        [Fact]
+        public void GetAddBookMethodShouldAuthorizeFilter()
+        {
+            MyController<BooksController>
+                .Instance()
+                .Calling(c => c.AddBook())
+                .ShouldHave()
+                .ActionAttributes(attrs => attrs
+                    .RestrictingForAuthorizedRequests());
+        }
+
+        [Fact]
+        public void AddBookShouldReturnView()
+        {
+            MyController<BooksController>
+                .Instance()
+                .Calling(c => c.AddBook())
+                .ShouldReturn()
+                .View();
+        }
+
+        [Fact]
+        public void PostAddBookMethodShouldAuthorizeFilter()
+        {
+            MyController<BooksController>
+                .Instance()
+                .Calling(c => c.AddBook(new AddBookFormModel()))
+                .ShouldHave()
+                .ActionAttributes(attrs => attrs
+                    .RestrictingForAuthorizedRequests());
+        }
+
+        [Fact]
+        public void AddBookShouldRedirectWithTempDataMessageAndSaveBookWithValidData()
+        {
+            var expectedModel = AddBookModel;
+
+            MyController<BooksController>
+                .Instance()
+                .WithUser(u => u.InRole(UserRoleName))
+                .WithData(TestGenre)
+                .Calling(c => c.AddBook(expectedModel))
+                .ShouldHave()
+                .Data(data => data
+                    .WithSet<Book>(set =>
+                    {
+                        set.ShouldNotBeNull();
+                        set
+                            .FirstOrDefault(s => s.GenreId == TestGenre.Id)
+                            .ShouldNotBeNull();
+                        set.FirstOrDefault(s => s.UserId == TestBook.UserId)
+                            .ShouldNotBeNull();
+                    }))
+                .AndAlso()
+                .ShouldHave()
+                .TempData(tempData => tempData
+                    .ContainingEntryWithKey(SuccessfullyAddedBookKey))
+                .AndAlso()
+                .ShouldReturn()
+                .RedirectToAction("MyLibrary");
+        }
+
+        [Fact]
+        public void EditBookMethodShouldAuthorizeFilter()
+        {
+            MyController<BooksController>
+                .Instance()
+                .Calling(c => c.EditBook(null))
+                .ShouldHave()
+                .ActionAttributes(attrs => attrs
+                    .RestrictingForAuthorizedRequests());
+        }
+
+        [Fact]
+        public void EditBookShouldReturnView()
+        {
+            MyController<BooksController>
+                .Instance()
+                .Calling(c => c.EditBook(null))
+                .ShouldReturn() 
+                .View();
+        }
+
+        [Fact]
+        public void PostEditMethodShouldAuthorizeFilter()
+        {
+            MyController<BooksController>
+                .Instance()
+                .Calling(c => c.Edit(new EditBookFormModel()))
+                .ShouldHave()
+                .ActionAttributes(attrs => attrs
+                    .RestrictingForAuthorizedRequests());
+        }
+
+        [Fact]
+        public void EditShouldRedirectWithTempDataMessageAndSaveEditedBookWithValidData()
+        {
+            var editedModel = EditedBookModel;
+
+            MyController<BooksController>
+                .Instance()
+                .WithUser(u => u.InRole(UserRoleName))
+                .WithData(TestBook, TestGenre)
+                .Calling(c => c.Edit(editedModel))
+                .ShouldHave()
+                .Data(data => data
+                    .WithSet<Book>(set =>
+                    {
+                        var newTitle = set.First().Title;
+                        newTitle.ShouldNotBeSameAs(TestBook.Title);
+                    }))
+                .AndAlso()
+                .ShouldHave()
+                .TempData(tempData => tempData
+                    .ContainingEntryWithKey(SuccessfullyEditedBookKey))
+                .AndAlso()
+                .ShouldReturn()
+                .RedirectToAction("All");
+        }
+
+        [Fact]
+        public void DeleteMethodShouldAuthorizeFilter()
+        {
+            MyController<BooksController>
+                .Instance()
+                .Calling(c => c.Delete(null))
+                .ShouldHave()
+                .ActionAttributes(attrs => attrs
+                    .RestrictingForAuthorizedRequests());
+        }
+
+        [Fact]
+        public void DeleteShouldRedirectWithTempDataMessageAndDeleteTheGivenBookByValidId()
+        {
+            MyController<BooksController>
+                .Instance()
+                .WithUser(u => u.InRole(UserRoleName))
+                .WithData(TestBook)
+                .Calling(c => c.Delete(TestBook.Id))
+                .ShouldHave()
+                .Data(data => data
+                    .WithSet<Book>(set => set.ShouldBeEmpty()))
+                .AndAlso()
+                .ShouldHave()
+                .TempData(tempData => tempData
+                    .ContainingEntryWithKey(SuccessfullyDeletedBookKey))
+                .AndAlso()
+                .ShouldReturn()
+                .RedirectToAction("All");
+        }
+
+        [Fact]
+        public void MyLibraryShouldReturnViewWithModelWithValidData()
+        {
+            var expectedModel = AllBooksModel;
+
+            MyController<BooksController>
+                .Instance()
+                .WithUser()
+                .WithData(TestBook)
+                .Calling(c => c.MyLibrary(1))
+                .ShouldReturn()
+                .View(expectedModel);
+        }
+    }
+}
---
+++ Library/Library.Tests/Data/DbModels/BooksControllerTestDbModels.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using Library.Services.Books.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Library.Tests.Data.DbModels
+{
+    using Library.Data.Models;
+
+    public class BooksControllerTestDbModels
+    {
+        public static Book TestBook => new()
+        {
+            Id = "TestBookId",
+            Author = "TestAuthor",
+            Title = "TestTitle",
+            GenreId = TestGenre.Id,
+            ImageUrl = "https://i.imgur.com/OTLkRq4.jpg",
+            LongDescription =
+                "Morbi convallis ex turpis, sit amet imperdiet nisl fringilla eget. Integer malesuada vitae nisl quis feugiat. Aenean eget magna eget libero finibus elementum. Aenean tincidunt elit sit amet felis egestas viverra. Fusce sit amet urna est. Vivamus eu tellus ac felis tempus pretium vel sit amet ante.",
+            ShortDescription =
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sollicitudin facilisis fringilla.",
+            UserId = "TestId"
+        };
+
+        public static Genre TestGenre => new()
+        {
+            Id = "TestGenreId",
+            Name = "TestGenre"
+        };
+
+        public static AllBooksServiceModel AllBooksModel = new()
+        {
+            CurrentPage = 1,
+            MaxPage = 1,
+            Books = new List<BookListingServiceModel>()
+            {
+                new()
+                {
+                    Id = TestBook.Id,
+                    Author = TestBook.Author,
+                    Title = TestBook.Title,
+                    ShortDescription = TestBook.ShortDescription,
+                    ImageUrl = TestBook.ImageUrl
+                }
+            }
+        };
+
+        public static BookDetailsServiceModel BookDetailsModel = new()
+        {
+            Id = TestBook.Id,
+            Author = TestBook.Author,
+            Title = TestBook.Title,
+            ImageUrl = TestBook.ImageUrl,
+            LongDescription = TestBook.LongDescription,
+            Genre = TestGenre.Name
+        };
+
+        public static AddBookFormModel AddBookModel = new()
+        {
+            Author = TestBook.Author,
+            Title = TestBook.Title,
+            GenreId = TestBook.GenreId,
+            ShortDescription = TestBook.ShortDescription,
+            LongDescription = TestBook.LongDescription,
+            ImageUrl = TestBook.ImageUrl
+        };
+
+        public static EditBookFormModel EditedBookModel = new()
+        {
+            Id = TestBook.Id,
+            Title = "Test Book New Title",
+            Author = TestBook.Author,
+            CurrentGenre = TestGenre.Name,
+            GenreId = TestBook.GenreId,
+            ShortDescription = TestBook.ShortDescription,
+            LongDescription = TestBook.LongDescription,
+            ImageUrl = TestBook.ImageUrl
+        };
+    }
+}
