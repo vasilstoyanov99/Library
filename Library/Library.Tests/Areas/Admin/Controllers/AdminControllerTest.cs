8403f0bcd1e69b489501204b6a799a1496e9f991
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-11-24 23:48:51 +0200

    Added admin controller test

---
+++ Library/Library.Tests/Areas/Admin/Controllers/AdminControllerTest.cs
@@ -0,0 +1,23 @@
+﻿namespace Library.Tests.Areas.Admin.Controllers
+{
+    using MyTested.AspNetCore.Mvc;
+    using Xunit;
+
+    using Library.Areas.Admin.Controllers;
+
+    using static Library.Areas.Admin.AdminConstants;
+
+    public class AdminControllerTest
+    {
+        [Fact]
+        public void ControllerShouldHaveAuthorizeFilter()
+            => MyMvc
+                .Controller<AdminController>()
+                .ShouldHave()
+                .Attributes(attrs =>
+                {
+                    attrs.RestrictingForAuthorizedRequests();
+                    attrs.SpecifyingArea(AdminAreaName);
+                });
+    }
+}
